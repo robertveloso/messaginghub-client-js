18caa85e14584d3331fa641c4e2900322745082c
Arthur Xavier <arthur.xavierx@gmail.com>
2015-12-22 17:25:47 -0200

    fix(test): Make MessagingHubClient tests pass
    Create TcpLimeServer helper which provides a basic LIME connection pipeline
    
    fix #5, ref #2

--- test/helpers/TcpTransport.js
+++ test/helpers/TcpTransport.js
@@ -3,6 +3,8 @@
 import net from 'net';
 import {Lime} from 'lime-js';
 
+console.debug = console.debug || console.log;
+
 export default class TcpTransport {
 
     constructor(traceEnabled) {
@@ -11,16 +13,16 @@ export default class TcpTransport {
         this._socket.on('close', this.onClose);
         this._socket.on('data', (e) => {
             if (this._traceEnabled) {
-                console.debug("TcpTransport RECEIVE: " + e.data);
+                console.debug("TcpTransport RECEIVE: " + e);
             }
-            this.onEnvelope(JSON.parse(e.data));
+            this.onEnvelope(JSON.parse(e));
         });
     }
 
     send(envelope) {
         var envelopeString = JSON.stringify(envelope);
         this._socket.write(envelopeString);
-        if (this.traceEnabled) {
+        if (this._traceEnabled) {
             console.debug("TcpTransport SEND: " + envelopeString);
         }
     }
@@ -28,9 +30,10 @@ export default class TcpTransport {
     onEnvelope(envelope) { }
 
     open(uri) {
+        var host = uri.split(':');
         this.encryption = Lime.SessionEncryption.none;
         this.compression = Lime.SessionCompression.none;
-        this._socket.connect(uri, this.onOpen);
+        this._socket.connect(host[1], host[0], this.onOpen);
     }
 
     close() {
--- test/messagingHubClientTest.js
+++ test/messagingHubClientTest.js
@@ -1,9 +1,15 @@
 import MessagingHubClient from '../src/MessagingHubClient';
-import TcpTransport from './helpers/TcpTransport'
+import TcpTransport from './helpers/TcpTransport';
+import TcpLimeServer from './helpers/TcpLimeServer';
 
 describe("MessagingHubClient tests", () => {
+
+    before((done) => {
+        TcpLimeServer.listen(8124, done);
+    });
+
     it("should execute callback without error after connect", (done) => {
-        var client = new MessagingHubClient("ws://msging.net:8081", new TcpTransport());
+        var client = new MessagingHubClient("127.0.0.1:8124", new TcpTransport());
         client.connect("", "", done);
     });
 });
