ad5252fefd8cdcc76149ac361418d9d2a6334c21
ridermanb <ridermanb@takenet.com.br>
2015-12-23 13:54:17 -0200

    chore(tool): Fix wallaby runner
    Stop using Webpack and remove wrong reference to sinon in wallaby configuration

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -6,47 +6,37 @@ var path = require('path');
 var babelConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc')));
 babelConfig.babel = babel;
 
-var wallabyWebpack = require('wallaby-webpack');
-var webpackConfig = require('./webpack.config');
-// removing babel-loader, we will use babel compiler instead, it's more performant
-webpackConfig.module.loaders = webpackConfig.module.loaders.filter(function(l){
-    return l.loader !== 'babel-loader';
-});
-delete webpackConfig.devtool;
-var wallabyPostprocessor = wallabyWebpack(webpackConfig);
-
 module.exports = function (wallaby) {
     return {
         files: [
-          {pattern: 'node_modules/chai/chai.js', instrument: false},
-          {pattern: 'node_modules/sinon/lib/sinon.js', instrument: false},
-          {pattern: 'node_modules/sinon/lib/**/*.js', load: false},
-          {pattern: 'test/helpers/*.js', instrument: false},
-          {pattern: 'src/**/*.js', load: false}
+          'node_modules/chai/chai.js',
+          'test/helpers/*.js',
+          'src/**/*.js'
         ],
 
         tests: [
-            {pattern: 'test/**/*Test.js', load: false}
+            'test/**/*Test.js'
         ],
-
-        env: {type: 'node'},
 
         debug: true,
 
         testFramework: 'mocha',
 
-        preprocessors: { '**/*.js': file => babel.transform(file.content, {sourceMap: true}) },
-
         compilers: {
             '**/*.js': wallaby.compilers.babel(babelConfig)
         },
 
-        postprocessor: wallabyPostprocessor,
+        env: {
+          type: 'node',
+          params: {
+            runner: '--harmony --harmony_arrow_functions',
+            env: 'TEST=true'
+          }
+        },
 
         bootstrap: function (wallaby) {
             var mocha = wallaby.testFramework;
             mocha.ui('bdd');
-            window.__moduleBundler.loadTests();
         }
     };
 };
