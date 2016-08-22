94999fb9e5a437254e1f9b57b087488b01c161f5
Arthur Xavier <arthur.xavierx@gmail.com>
2016-08-22 18:04:34 -0300

    feat(MessagingHubClient): reject the promise of a command when the received status is 'failure'

--- src/MessagingHubClient.js
+++ src/MessagingHubClient.js
@@ -121,9 +121,13 @@ export default class MessagingHubClient {
     // sendCommand :: Command -> Promise Command
     sendCommand(command) {
         this._clientChannel.sendCommand(command);
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             this._commandResolves[command.id] = (c) => {
-                resolve(c);
+                if (c.status && c.status === Lime.CommandStatus.SUCCESS) {
+                    resolve(c);
+                } else {
+                    reject(c);
+                }
                 delete this._commandResolves[command.id];
             };
         });
